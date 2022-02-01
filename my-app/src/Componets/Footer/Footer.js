ee53a88f01e22c83c7d87c45cb60ec9424aa5428
Lenine Xavier <leninecxavier@gmail.com>
2022-02-01 09:04:29 -0300

    done

--- my-app/src/Componets/Footer/Footer.js
+++ my-app/src/Componets/Footer/Footer.js
@@ -6,13 +6,32 @@ import icone4 from '../../images/icon4.png';
 
 function Footer(){
     return(
-        <nav className={styles.footer}>
+        
+        <nav className={styles.Footer}>
+            <div>
         <img src={icone1} alt='ironlogo'/>
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interactive UIs</p>
+        </div>
+
+            <div>
         <img src={icone2} alt='ironlogo'/>
+        <h2>Componets</h2>
+        <p>Build encapsulated components that manage their state</p>
+        </div>
+
+            <div>
         <img src={icone3} alt='ironlogo'/>
-                  
+        <h2>Single-Way</h2>
+        </div>
+
+        <div>
         <img src={icone4} alt='menu'/> 
+        <h2>JSX</h2>
+        </div> 
         </nav>
+
+        
     )
 }
 export default Footer;
